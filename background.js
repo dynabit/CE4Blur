6c13d401a8c815aaa1f98c43ad214f6087ccc105
dynabit <dynabit@users.noreply.github.com>
2023-02-21 23:50:21 +0800

    Update background.js

--- background.js
+++ background.js
@@ -9,13 +9,14 @@ chrome.runtime.onMessage.addListener(
       	openCollectionPage(request.name);
     }
   	else chrome.tabs.sendMessage(tabid,request);
+  	sendResponse({msg: "ok"});
   }
 );
 function openCollectionPage(name){
 	chrome.tabs.create({url: getUrl(name), active: false});
 }
 function getUrl(name){
-	var mapping={"Azuki":"azuki"};
+	var mapping={"Azuki":"azuki","MutantApeYachtClub":"mutant-ape-yacht-club"};
 	var path=mapping[name];
 	if(path==undefined)path=name;
 	return 'https://blur.io/collection/'+path+'/bids';
